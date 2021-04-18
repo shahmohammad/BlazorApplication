d12f8534719a22f5d56fee22a6ccc8a37228cba1
Mohammad Shah <mohammad.shah@aurea.com>
2021-04-18 16:28:28 +0530

    First commit

---
+++ EmployeeManagement.Web/Pages/DisplayEmployeeBase.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class DisplayEmployeeBase : ComponentBase
+    {
+        [Parameter]
+        public Employee Employee { get; set; }
+
+        [Parameter]
+        public bool ShowFooter { get; set; }
+
+        protected bool IsSelected { get; set; }
+
+        [Parameter]
+        public EventCallback<bool> OnEmployeeSelection { get; set; }
+
+        protected async Task CheckBoxChanged(ChangeEventArgs e)
+        {
+            IsSelected = (bool)e.Value;
+            await OnEmployeeSelection.InvokeAsync(IsSelected);
+        }
+    }
+}
